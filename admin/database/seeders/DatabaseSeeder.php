c3519599d568e60059c668cf697a39dea7fe7fca
geekbim <manyuabim9@gmail.com>
2021-05-09 16:37:59 +0700

    laravel microservices

--- admin/database/seeders/DatabaseSeeder.php
+++ admin/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        for ($i=1; $i <= 20; $i++) { 
+            User::insert([
+                'id' => $i
+            ]);
+        }
+
+        $this->call([
+            // UserSeeder::class,
+            ProductSeeder::class
+        ]);
     }
 }
